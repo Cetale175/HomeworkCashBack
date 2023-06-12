45a1bcb963993ee34994d971efbcabadf8924cfe
Kirylbas <kirill.kolobov.98@gmail.com>
2023-06-12 19:39:32 +0300

    initial commit

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,26 +1,24 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashBackHackerTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashBackHacker service = new CashBackHacker();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashBackHacker service = new CashBackHacker();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
